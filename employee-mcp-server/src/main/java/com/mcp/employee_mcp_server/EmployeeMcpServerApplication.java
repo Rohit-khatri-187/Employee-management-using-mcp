fe59074c85c05bdd16f96d8773ac5b3ae04bd7b9
Rohit-khatri-187 <khatrirohit1000@gmail.com>
2026-07-08 11:44:19 +0545

    first commit

---
+++ employee-mcp-server/src/main/java/com/mcp/employee_mcp_server/EmployeeMcpServerApplication.java
@@ -0,0 +1,16 @@
+package com.mcp.employee_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMcpServerApplication.class, args);
+
+		System.out.println("Employee mcp server is started : ");
+
+	}
+
+}
